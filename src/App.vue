387470ddbc7e7cb5c41f81fe811f0b9180b868b4
Samuel Stoltenberg <samuel@taubyte.com>
2022-08-30 01:10:13 -0500

    playing with yjs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainView/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainView from './components/main-view.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainView
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+const path = require("path");
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new MonacoWebpackPlugin({
+        languages: ["python"],//configure your languages here
+        features: ["coreCommands", "find"],
+      }), // Place it here
+    ],
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set(
+      "vscode",
+      path.resolve(
+        "./node_modules/monaco-languageclient/lib/vscode-compatibility"
+      )
+    );
+  },
+};
