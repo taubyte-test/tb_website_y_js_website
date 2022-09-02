dd148ba11ccedb2ddd2996b94d5089eb5705a1b6
Samuel Stoltenberg <samuel@taubyte.com>
2022-09-02 12:08:11 -0500

    working y_js

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,5 @@
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-const path = require("path");
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new MonacoWebpackPlugin({
-        languages: ["python"],//configure your languages here
-        features: ["coreCommands", "find"],
-      }), // Place it here
-    ],
-  },
-  chainWebpack: (config) => {
-    config.resolve.alias.set(
-      "vscode",
-      path.resolve(
-        "./node_modules/monaco-languageclient/lib/vscode-compatibility"
-      )
-    );
-  },
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tiptap/dist/'
+    : '/'
+}
