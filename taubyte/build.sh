2c59465d896c56e0296e2a49f7a10bd22800d5cd
Samuel Stoltenberg <samuel@taubyte.com>
2022-09-04 19:16:32 -0500

    Update script

--- taubyte/build.sh
+++ taubyte/build.sh
@@ -2,9 +2,14 @@
 
 export NODE_OPTIONS=--openssl-legacy-provider
 
+#!/bin/bash
+
 cd monaco
 npm install
 npm run dist
 mkdir ../out
-mv dist/* ../out
+cp -r dist/* ../out
 cp index.html ../out
+
+cd ..
+cp -r monaco out
